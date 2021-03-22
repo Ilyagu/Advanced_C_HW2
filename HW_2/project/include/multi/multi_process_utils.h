5a9867d7a69d3cc3086b773d0c6bc2610b24d46b
Ilyagu <inagdimaev@mail.ru>
2021-03-23 01:30:31 +0300

    init

---
+++ HW_2/project/include/multi/multi_process_utils.h
@@ -0,0 +1,19 @@
+// Copyright 2021 Ilyagu Nagdimaev
+
+#ifndef HW_2_PROJECT_INCLUDE_MULTI_MULTI_PROCESS_UTILS_H_
+#define HW_2_PROJECT_INCLUDE_MULTI_MULTI_PROCESS_UTILS_H_
+
+#include <pthread.h>
+#include "utils.h"
+
+# define PARENT_PID -5
+
+typedef struct {
+    int main_diagonal;
+    int side_diagonal;
+    pthread_mutex_t mutex;
+} Calculation_multi_proc_res;
+
+Calculation_res* multi_process(Matrix* matrix);
+
+#endif  // HW_2_PROJECT_INCLUDE_MULTI_MULTI_PROCESS_UTILS_H_
---
+++ HW_2/project/include/one/one_proc_utils.h
@@ -0,0 +1,10 @@
+// Copyright 2021 Ilyagu Nagdimaev
+
+#ifndef HW_2_PROJECT_INCLUDE_ONE_ONE_PROC_UTILS_H_
+#define HW_2_PROJECT_INCLUDE_ONE_ONE_PROC_UTILS_H_
+
+#include "utils.h"
+
+Calculation_res* calculate_matrix(Matrix* matrix);
+
+#endif  // HW_2_PROJECT_INCLUDE_ONE_ONE_PROC_UTILS_H_
